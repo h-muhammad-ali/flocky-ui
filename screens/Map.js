1eae42c83aa802a75800961a99cbbd9f9492434c
Muhammad Ali <muhammadalih514@gmail.com>
2022-05-18 19:55:38 +0500

    Added Current Location Functionality + Fine-tuned Map's Zoom Level for better Location Selection

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import MapView from "react-native-maps";
-import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import { Ionicons, FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import Header from "../components/Header";
 import { GOOGLE_MAPS_API_KEY } from "@env";
 import { useDispatch } from "react-redux";
@@ -10,28 +10,100 @@ import {
   setDestination,
   setWayPoint,
 } from "../redux/locations/locationsActions";
+import * as Location from "expo-location";
+import usePrevious from "../custom-hooks/usePrevious";
+var _ = require("lodash");
 
 const Map = ({ navigation, route }) => {
   const [location, setLocation] = useState(null);
   const [address, setAddress] = useState("");
+  const mapRef = useRef(null);
+  const prevLocation = usePrevious(location);
   const dispatch = useDispatch();
   const onRegionChangeComplete = (region) => {
     setAddress("Loading...");
-    fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${region?.latitude},${region?.longitude}&key=${GOOGLE_MAPS_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((responseJson) => {
-        let result = responseJson["results"][0];
-        setAddress(result["formatted_address"]);
-
-        setLocation({
-          coords: result["geometry"]["location"],
-          place_id: result["place_id"],
-          formatted_address: result["formatted_address"],
-          short_address: `${result["address_components"][0]["long_name"]}, ${result["address_components"][1]["short_name"]}`,
+    if (location === null && prevLocation === undefined) {
+      getCurrentPosition();
+    } else {
+      fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${region?.latitude},${region?.longitude}&key=${GOOGLE_MAPS_API_KEY}`
+      )
+        .then((response) => response.json())
+        .then((responseJson) => {
+          let result = responseJson["results"][0];
+          setAddress(result["formatted_address"]);
+          setLocation({
+            coords: result["geometry"]["location"],
+            place_id: result["place_id"],
+            formatted_address: result["formatted_address"],
+            short_address: `${result["address_components"][0]["long_name"]}, ${result["address_components"][1]["short_name"]}`,
+          });
         });
-      });
+    }
+  };
+  const getCurrentPosition = async (isAnimate = false) => {
+    Location.requestForegroundPermissionsAsync().then((response) => {
+      if (response?.status === "granted") {
+        Location.getCurrentPositionAsync({})
+          .then((response) => {
+            fetch(
+              "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+                response?.coords?.latitude +
+                "," +
+                response?.coords?.longitude +
+                "&key=" +
+                GOOGLE_MAPS_API_KEY
+            )
+              .then((response) => response.json())
+              .then((responseJson) => {
+                let result = responseJson["results"][0];
+                setAddress(result["formatted_address"]);
+                setLocation({
+                  coords: result["geometry"]["location"],
+                  place_id: result["place_id"],
+                  formatted_address: result["formatted_address"],
+                  short_address: `${result["address_components"][0]["long_name"]}, ${result["address_components"][1]["short_name"]}`,
+                });
+                if (isAnimate) {
+                  const coords = result["geometry"]["location"];
+                  mapRef?.current?.animateToRegion(
+                    {
+                      latitude: coords?.lat,
+                      longitude: coords?.lng,
+                      latitudeDelta: 0.01,
+                      longitudeDelta: 0.01,
+                    },
+                    2000
+                  );
+                }
+              });
+          })
+          .catch((error) => {
+            console?.log(error?.message);
+          });
+      } else {
+        setErrorMsg("Permission to access location was denied");
+        dispatch(setSource(null));
+      }
+    });
+  };
+  useEffect(() => {
+    if (prevLocation === null) {
+      setTimeout(() => {
+        animateRegion();
+      }, 1000);
+    }
+  }, [location]);
+  const animateRegion = () => {
+    mapRef?.current?.animateToRegion(
+      {
+        latitude: location?.coords?.lat,
+        longitude: location?.coords?.lng,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      },
+      2000
+    );
   };
   return (
     <View style={[StyleSheet?.absoluteFillObject, styles?.container]}>
@@ -45,6 +117,7 @@ const Map = ({ navigation, route }) => {
           longitudeDelta: 12,
         }}
         onRegionChangeComplete={onRegionChangeComplete}
+        ref={mapRef}
       />
       <View style={styles?.heading}>
         <Header
@@ -58,6 +131,15 @@ const Map = ({ navigation, route }) => {
       <View style={styles.textView}>
         <Text>{address}</Text>
       </View>
+      <TouchableOpacity
+        style={styles?.currentLocation}
+        onPress={() => {
+          if (location?.place_id !== prevLocation?.place_id)
+            getCurrentPosition(true);
+        }}
+      >
+        <MaterialIcons name="my-location" size={60} color="black" />
+      </TouchableOpacity>
       <TouchableOpacity
         style={styles?.button}
         onPress={() => {
@@ -129,4 +211,14 @@ const styles = StyleSheet.create({
     width: 80,
   },
   heading: { position: "absolute", top: 0, left: 0, right: 0, bottom: 0 },
+  currentLocation: {
+    flex: 0,
+    position: "absolute",
+    right: 30,
+    bottom: 100,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    height: 80,
+    width: 80,
+  },
 });
